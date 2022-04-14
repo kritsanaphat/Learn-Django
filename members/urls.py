0d8bd490ad4d279942c002841cf747e79df3f22f
Kritsanaphat <63010040@kmitl.ac.th>
2022-04-14 18:26:32 +0700

    'add Record'

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path(''     , views.home , name ='home'),
+    path('about', views.about, name ='about'),
+    path('index', views.index, name ='index'),
+    path('add'  , views.add  , name = 'add'),
+    path('add/addrecord', views.addrecord, name='addrecord'),
 ]
