64842cc1c8e1517eb8569c5ee9c5bf432442bf56
Kritsanaphat <63010040@kmitl.ac.th>
2022-06-06 11:14:26 +0700

    view and templates

--- members/urls.py
+++ members/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(''     , views.home , name ='home'),
+    path('', views.home , name ='home'),
     path('about', views.about, name ='about'),
     path('index', views.index, name ='index'),
     path('add'  , views.add  , name = 'add'),
     path('add/addrecord', views.addrecord, name='addrecord'),
+    path('<str:id>',views.id,name='id') #dynamic link
 ]
--- members/views.py
+++ members/views.py
@@ -10,25 +10,26 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Members
 from django.urls import reverse
+import uuid
 
 def home(request):
-  template = loader.get_template('myfirst.html')
+  template = loader.get_template('general/myfirst.html')
   return HttpResponse(template.render())
   
 def about(requset):
-  template = loader.get_template('about.html')
+  template = loader.get_template('general/about.html')
   return HttpResponse(template.render())
 
 def index(request):
   mymembers = Members.objects.all().values()
-  template = loader.get_template('index.html')
+  template = loader.get_template('general/index.html')
   context = {
     'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
 
 def add(request):
-  template = loader.get_template('add.html')
+  template = loader.get_template('general/add.html')
   return HttpResponse(template.render({}, request))
 
 def addrecord(request):
@@ -36,15 +37,7 @@ def addrecord(request):
   y = request.POST['last']
   member = Members(firstname=x, lastname=y)
   member.save()
-  return HttpResponseRedirect(reverse('index'))
-#--------------
-# from django.http import HttpResponse
-# from django.template import loader
-# from .models import Members
+  return HttpResponseRedirect(reverse('general/index'))
 
-# def index(request):
-#   mymembers = Members.objects.all().values()
-#   output = ""
-#   for x in mymembers:
-#     output += x["firstname"]
-#   return HttpResponse(output)
+def id(requset,id):
+  return HttpResponse('ชื่อผู้ใช้งาน = '+str(id))
