b0046ce46bd104a14977106b1a928523dea6f439
Kritsanaphat <63010040@kmitl.ac.th>
2022-06-08 21:36:23 +0700

    tag2/2

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.books , name ='book'),
-    
+    path('', views.books , name ='books'),
+    path('<str:book_id>',views.book,name='book') #dynamic link
 ]
--- books/views.py
+++ books/views.py
@@ -1,8 +1,22 @@
+from multiprocessing import context
 from re import template
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
-def books(request):
+all_book = [
+  {'id' : 1,'title': 'Physic','price':200,'is_premium': True },
+  {'id' : 2,'title': 'Math','price':150,'is_premium': False },
+  {'id' : 3,'title': 'Sport','price':100,'is_premium': False }
+]
+
+def book(request,book_id):
   template = loader.get_template('general/book.html')
-  return HttpResponse(template.render())
+  return HttpResponse(template.render(context={'book_id':book_id}))
+
+def books(request):
+  context = {'books': all_book}
+  template = loader.get_template('general/books.html')
+  return HttpResponse(template.render(context))
+
+
