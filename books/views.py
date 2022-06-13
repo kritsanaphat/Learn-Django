8e48f30aff083e57645133cc3b6cf42dc2bfb2c7
Kritsanaphat <63010040@kmitl.ac.th>
2022-06-13 17:08:59 +0700

    end url,views,templates,tag,filters

--- books/views.py
+++ books/views.py
@@ -3,12 +3,18 @@ from re import template
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-
+from datetime import datetime
 
 all_book = [
-  {'id' : 1,'title': 'Physic','price':200,'is_premium': True },
-  {'id' : 2,'title': 'Math','price':150,'is_premium': False },
-  {'id' : 3,'title': 'Sport','price':100,'is_premium': False },
+  {'id' : 1,'title': 'Physic','price':1200,'is_premium': True,
+    'pmt_end': datetime(2022,2,28)
+   },
+  {'id' : 2,'title': 'Math','price':150,'is_premium': False, 
+    'pmt_end': datetime(2022,2,28)
+  },
+  {'id' : 3,'title': 'Sport','price':100,'is_premium': False,
+    'pmt_end': datetime(2022,2,28)
+   },
    
 ]
 
@@ -19,7 +25,11 @@ def books(request):
 
 
 def book(request, book_id):
-  one_book = [f for f in all_book if f['id'] == book_id][0]
+  one_book = None
+  try:
+    one_book = [f for f in all_book if f['id'] == book_id][0]
+  except IndexError:
+    print("ไม่พบข้อมูล")
   context = {'book':one_book}
   template = loader.get_template('general/book.html')
   return HttpResponse(template.render(context))
--- myworld/settings.py
+++ myworld/settings.py
@@ -106,9 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'th'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
