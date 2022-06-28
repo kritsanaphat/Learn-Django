c9c8a6e9b7c1603e92629ede2accbe67097fd1b2
Kritsanaphat <63010040@kmitl.ac.th>
2022-06-28 22:24:32 +0700

    create adminBook

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from books.models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'special_price','is_premium']
+    search_fields =  ['title']
+    list_filter = ['is_premium']
+
+
+admin.site.register(Book,BookAdmin)
+
--- books/models.py
+++ books/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=60)
     price = models.IntegerField()
-    special_price = models.IntegerField(null=True)
-    is_premium = models.BooleanField(default=False)
-    pmt_end = models.DateTimeField(null=True)
+    special_price = models.IntegerField(null=True,blank=True)
+    is_premium = models.BooleanField(default=False,blank=True)
+    pmt_end = models.DateTimeField(null=True,blank=True)
     description = models.TextField(null=True)
     
     def __str__(self) -> str:
