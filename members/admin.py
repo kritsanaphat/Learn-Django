2ddc333510ed7c7457286d8c6a5ff22077b85ba7
Kritsanaphat <63010040@kmitl.ac.th>
2022-06-28 22:29:35 +0700

    create adminMember

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from members.models import Members
+
 # Register your models here.
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ['firstname', 'lastname', 'status','register_at']
+    search_fields =  ['firstname','lastname']
+    list_filter = ['status']
+
+admin.site.register(Members,MembersAdmin)
